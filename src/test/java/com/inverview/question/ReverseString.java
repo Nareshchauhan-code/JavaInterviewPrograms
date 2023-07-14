74a7b0e0423f69a585fc1c0cbffcea7d52b04120
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-07-15 03:59:37 +0530

    Done

---
+++ src/test/java/com/inverview/question/ReverseString.java
@@ -0,0 +1,8 @@
+package com.inverview.question;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/test/java/com/test/HelloPrintSeprateLine.java
+++ src/test/java/com/test/HelloPrintSeprateLine.java
@@ -1,4 +1,4 @@
-package com.java;
+package com.test;
 
 public class HelloPrintSeprateLine {
 
