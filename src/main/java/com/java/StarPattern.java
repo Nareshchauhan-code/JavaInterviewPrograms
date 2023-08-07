451a3cfc4fc197c6fdf1bc0e47cd2ed0743dfc80
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-07 18:21:15 +0530

    StarPattern

---
+++ src/main/java/com/java/StarPattern.java
@@ -0,0 +1,28 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class StarPattern {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter the Value ");
+        int num = sc.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+
+        }
+
+
+    }
+
+
+}
