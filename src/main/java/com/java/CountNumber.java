5f054c7aa270c1ff0dcb38116e39460aed29d9fa
Nareshchauhan-code <nareshchauhan2014@gmail.com>
2023-08-08 20:10:05 +0530

    CountNumber

---
+++ src/main/java/com/java/CountNumber.java
@@ -0,0 +1,28 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class CountNumber {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please Enter the Number");
+
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        while (num > 0) {
+
+            num = num / 10;
+
+            count++;
+
+        }
+
+        System.out.println(count);
+
+    }
+}
